ebf9022e4b12c7bf1723673265a1089d7e561ef3
krunalsukhwani <68172724+krunalsukhwani@users.noreply.github.com>
2023-07-19 10:54:14 -0400

    Register student

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  firstname: string="";
+  lastname: string="";
+  email: string="";
+  password: string="";
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void{
+
+  }
+
+  register(){
+    let bodyData = {
+      "firstname" : this.firstname,
+      "lastname" : this.lastname,
+      "email" : this.email,
+      "password" : this.password,
+    };
+
+    this.http.post("http://localhost:9992/student/create", bodyData).subscribe((resultData: any) => {
+      console.log(resultData);
+      alert("Student Registered Successfully");
+    });
+  }
+
+  save(){
+    this.register();
+  }
 
 }
